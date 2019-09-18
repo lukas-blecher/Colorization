d1e0565aee075885ec474f0f560a027eba6c7145
chrislbie <christopher.lueken.winkels@gmail.com>
2019-09-18 16:51:40 +0200

    debugging

--- train_patch_gan.py
+++ train_patch_gan.py
@@ -75,7 +75,7 @@ def main(argv):
     if data_path == './cifar-10':
         in_size = 32
     elif 'places' in data_path:
-        in_size = 256
+        in_size = 224
     in_shape=(3,in_size,in_size)
 
     #out_shape=(s.classes,32,32)
